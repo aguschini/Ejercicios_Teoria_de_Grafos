dc6ac33e72f08e37b9cb75e71abbae1894e4395b
aguschini <agusfranchini10@gmail.com>
2025-05-26 22:08:05 +0000

    ejercicio 3

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import src.resolucion_practica as p1
 
 
 c = (['A','B','C','D'],[('A','B'),('B','C'),('C','B')])
+d = (['A','B','C','D','E'],[('A','B'),('B','C'),('C','B'),('D','E')])
 
-p1.cuenta_grado(c)
-p1.vertice_aislado(c)
+p1.componentes_conexas(d)
--- src/resolucion_practica.py
+++ src/resolucion_practica.py
@@ -35,3 +35,38 @@ def vertice_aislado(grafo_lista):
             verticecero[i] = vertice
             i += 1
             print(f"{verticecero}")
+
+
+
+def componentes_conexas(grafo_lista):
+    vertices = grafo_lista[0]
+    aristas = grafo_lista[1]
+
+    adyacencia = {v: [] for v in vertices}
+
+    for u, v in aristas:
+        adyacencia[u].append(v)
+        adyacencia[v].append(u) 
+    
+    visitados = set()
+    componentes = []
+
+    for vertice in vertices:
+        if vertice not in visitados:
+            componente = []
+            pila = [vertice]
+            
+            while pila:
+                v = pila.pop()
+                if v not in visitados:
+                    visitados.add(v)
+                    componente.append(v)
+                    for vecino in adyacencia[v]:
+                        if vecino not in visitados:
+                            pila.append(vecino)
+            
+            componentes.append(componente)
+    
+    print(f"{componentes}")
+    return componentes
+    
