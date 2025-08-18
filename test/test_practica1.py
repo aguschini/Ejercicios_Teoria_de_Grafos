1a0a9ea3e30b52420d5d180cae0c044069bcbc10
aguschini <agusfranchini10@gmail.com>
2025-08-18 21:42:53 +0000

    cambios de entorno

--- test/test_practica1.py
+++ test/test_practica1.py
@@ -1,13 +1,13 @@
 # test.py
 import pytest
-from src.resolucion_practica import cuenta_grafo  # Cambiá 'tu_modulo' por el nombre real del archivo .py
+from src.practica1 import lee_grafo
 
-def test_lee_grafo():
+def test_lee_grafo_basico():
     entrada = ['3', 'A', 'B', 'C', 'A B', 'B C', 'C B']
     vertices_esperados = ['A', 'B', 'C']
     aristas_esperadas = [('A', 'B'), ('B', 'C'), ('C', 'B')]
     
-    vertices, aristas = test_lee_grafo(entrada)
+    vertices, aristas = lee_grafo(entrada)
     
     assert vertices == vertices_esperados
     assert aristas == aristas_esperadas
@@ -17,7 +17,7 @@ def test_lee_grafo_sin_aristas():
     vertices_esperados = ['X', 'Y']
     aristas_esperadas = []
     
-    vertices, aristas = test_lee_grafo(entrada)
+    vertices, aristas = lee_grafo(entrada)
     
     assert vertices == vertices_esperados
     assert aristas == aristas_esperadas
@@ -27,7 +27,8 @@ def test_lee_grafo_con_espacios_extra():
     vertices_esperados = ['A', 'B']
     aristas_esperadas = [('A', 'B'), ('B', 'A')]
     
-    vertices, aristas = test_lee_grafo(entrada)
+    vertices, aristas = lee_grafo(entrada)
     
     assert vertices == vertices_esperados
     assert aristas == aristas_esperadas
+    
